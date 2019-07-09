00668729a9251a35a3432a01718d5fc0b21a726d
Brian Hsu <brianhsu@berkeley.edu>
2019-07-08 21:46:54 -0700

    Converted from abbreviations to full names

--- js/bart.js
+++ js/bart.js
@@ -5,6 +5,55 @@ const SCHEDULE_ENDPOINT = "/sched.aspx?";
 const BERKELEY = "dbrk";
 const MONTGOMERY = "mont";
 
+const ABBREVS = {'12th': '12th St. Oakland City Center',
+    '16th': '16th St. Mission (SF)',
+    '19th': '19th St. Oakland',
+    '24th': '24th St. Mission (SF)',
+    'antc': 'Antioch',
+    'ashb': 'Ashby (Berkeley)',
+    'balb': 'Balboa Park (SF)',
+    'bayf': 'Bay Fair (San Leandro)',
+    'cast': 'Castro Valley',
+    'civc': 'Civic Center (SF)',
+    'colm': 'Colma',
+    'cols': 'Coliseum',
+    'conc': 'Concord',
+    'daly': 'Daly City',
+    'dbrk': 'Downtown Berkeley',
+    'deln': 'El Cerrito del Norte',
+    'dubl': 'Dublin/Pleasanton',
+    'embr': 'Embarcadero (SF)',
+    'frmt': 'Fremont',
+    'ftvl': 'Fruitvale (Oakland)',
+    'glen': 'Glen Park (SF)',
+    'hayw': 'Hayward',
+    'lafy': 'Lafayette',
+    'lake': 'Lake Merritt (Oakland)',
+    'mcar': 'MacArthur (Oakland)',
+    'mlbr': 'Millbrae',
+    'mont': 'Montgomery St. (SF)',
+    'nbrk': 'North Berkeley',
+    'ncon': 'North Concord/Martinez',
+    'oakl': "Oakland Int'l Airport",
+    'orin': 'Orinda',
+    'pctr': 'Pittsburg Center',
+    'phil': 'Pleasant Hill',
+    'pitt': 'Pittsburg/Bay Point',
+    'plza': 'El Cerrito Plaza',
+    'powl': 'Powell St. (SF)',
+    'rich': 'Richmond',
+    'rock': 'Rockridge (Oakland)',
+    'sanl': 'San Leandro',
+    'sbrn': 'San Bruno',
+    'sfia': "San Francisco Int'l Airport",
+    'shay': 'South Hayward',
+    'ssan': 'South San Francisco',
+    'ucty': 'Union City',
+    'warm': 'Warm Springs/South Fremont',
+    'wcrk': 'Walnut Creek',
+    'wdub': 'West Dublin',
+    'woak': 'West Oakland'};
+
 function getDepartures(fromStation, toStation, table) {
     params = {
         "cmd": "depart",
@@ -42,8 +91,12 @@ function makeRow(departureObject) {
     return {
         departureTime: departureObject["@origTimeMin"],
         destinationTime: departureObject["@destTimeMin"],
-        origin: departureObject["@origin"],
-        destination: departureObject["@destination"],
+        origin: toFullName(departureObject["@origin"]),
+        destination: toFullName(departureObject["@destination"]),
         numTransfers: departureObject.leg.length - 1
     }
+}
+
+function toFullName(abbrev) {
+    return ABBREVS[abbrev.toLowerCase()]
 }
